9de1df294b579a258f927fe4a62ea3f069e4ec70
east <694292191@qq.com>
2024-01-22 08:12:51 +0800

    init webpack-git-version-plugin

---
+++ README.md
@@ -0,0 +1,106 @@
+# webpack-git-version-plugin
+
+This plugin will generate a `version.json` file and add build information that contains build time、project name, version、branch、commit id、commit msg and so on into `index.html` head meta when webpack the current repository on a local git repository.
+ 
+## Usage
+
+install it as a local development dependency:
+
+  ```bash
+  npm install @east/webpack-git-version-plugin -D
+  ```
+
+Then, simply configure it as a plugin in the webpack config `vue.config.js`:
+
+  ```js
+  const WebpackGitVersionPlugin = require('@east/webpack-git-version-plugin');
+
+  module.exports = {
+    ...
+    configureWebpack: config => {
+      config.plugins.push(new WebpackGitVersionPlugin())
+    }
+    ...
+  }
+  ```
+
+This will generate a file `version.json`:
+
+  ```
+  {
+    "projectName": "xxx",
+    "projectVersion": "xxx",
+    "branchName": "xxx",
+    "commitId": "xxx",
+    "commitAuthor": "xxx",
+    "commitEmail": "xxx",
+    "commitTime": "xxx",
+    "commitMsg": "xxx",
+    "buildTime": "xxx",
+    "buildUserName": "xxx",
+    "buildUserEmail": "xxx"
+  }
+  ```
+
+and add meta into head `index.html`
+
+  ```
+  <head>
+  ...
+  <meta name="pkgInfo" content="buildTime:xxx version:xxx project:xxx branch:xxx commit:xxx">
+  <meta name="revised" content="buildUser:xxx buildEmail:xxx commitTime:xxx commitMsg:xxx commitAuthor:xxx commitEmail:xxx">
+  ...
+  </head>
+  ```
+
+## Configuration
+
+The plugin requires no configuration by default, but it is possible to configure it to support custom build info.
+
+### `isBuildFile: true`
+whether or not generate a `version.json` file in output directory, default true.
+
+### `outputFile: version.json`
+generate file name, default `version.json`
+
+### `indexFileName: index.html`
+add meta info into file name, default `index.html`
+
+### `replaceMeta: <meta name="buildversion">`
+the placeholder for replace real build info in indexFileName, default `<meta name="buildversion">`
+
+### `hiddenHead: false`
+whether or not hidden build info in head, default false.
+
+### `hiddenProjectName: false`
+whether or not hidden project name in head, default false.
+
+### `hiddenProjectVersion: false`
+whether or not hidden project version in head, default false.
+
+### `hiddenBranchName: false`
+whether or not hidden current branch name in head, default false.
+
+### `hiddenCommitId: false`
+whether or not hidden current commit id in head, default false.
+
+### `hiddenBuildTime: false`
+whether or not hidden current build time in head, default false.
+
+### `hiddenCommitAuthor: false`
+whether or not hidden current commit author in head, default false.
+
+### `hiddenCommitEmail: false`
+whether or not hidden current commit email in head, default false.
+
+### `hiddenCommitTime: false`
+whether or not hidden current commit time in head, default false.
+
+### `hiddenCommitMsg: false`
+whether or not hidden current commit message in head, default false.
+
+### `hiddenBuildUserName: false`
+whether or not hidden current commit user in head, default false.
+
+### `hiddenBuildUserEmail: false`
+whether or not hidden build user email  in head, default false.
---
+++ src/index.js
@@ -0,0 +1,156 @@
+// 同步子进程
+const execSync = require('child_process').execSync
+
+// 获取当前git分支信息
+function getGitVersionInfo() {
+  // 当前分支名 git name-rev --name-only HEAD 这个命令会在终端输出你当前的版本或标签信息。
+  const branchName = execSync('git name-rev --name-only HEAD').toString().trim()
+  // 最后一次提交的commit full hash
+  const commitId = execSync('git show -s --format=%H').toString().trim()
+  // 提交人姓名
+  const commitAuthor = execSync('git show -s --format=%cn').toString().trim()
+  // 提交人邮箱
+  const commitEmail = execSync('git show -s --format=%ce').toString().trim()
+  // 提交时间
+  const commitTime = new Date(new Date(execSync('git show -s --format=%cd').toString())).toLocaleString()
+  // 提交描述
+  const commitMsg = execSync('git show -s --format=%s').toString().trim()
+  // 构建时间
+  const buildTime = (new Date()).toLocaleString()
+  // 构建人姓名
+  const buildUserName = execSync('git config user.name').toString().trim()
+  // 构建人邮箱
+  const buildUserEmail = execSync('git config user.email').toString().trim()
+  return {
+    branchName: branchName,
+    commitId: commitId,
+    commitAuthor: commitAuthor,
+    commitEmail: commitEmail,
+    commitTime: commitTime,
+    commitMsg: commitMsg,
+    buildTime: buildTime,
+    buildUserName: buildUserName,
+    buildUserEmail: buildUserEmail
+  }
+}
+
+// 创建分支版本类
+class WebpackGitVersionPlugin {
+  defaultOptions = {
+    outputFile: 'version.json', // 版本信息保存文件名称
+    isBuildFile: true, // 是否生成版本信息文件
+    indexFileName: 'index.html', // 在head中增加打包版本信息文件名称
+    replaceMeta: '<meta name="buildversion">', // indexFileName文件中用于替换打包版本信息的占位
+    hiddenHead: false, // 是否在head中隐藏版本信息
+    hiddenProjectName: false,
+    hiddenProjectVersion: false,
+    hiddenBranchName: false,
+    hiddenCommitId: false,
+    hiddenBuildTime: false,
+    hiddenCommitAuthor: false,
+    hiddenCommitEmail: false,
+    hiddenCommitTime: false,
+    hiddenCommitMsg: false,
+    hiddenBuildUserName: false,
+    hiddenBuildUserEmail: false,
+  }
+  constructor(options) {
+    // options 为调用时传的参数
+    console.log('WebpackGitVersionPlugin 被调用！', options)
+    this.options = {...this.defaultOptions, ...options}
+  }
+  /**
+   * compiler: webpack的实例 所有的内容
+   * compilation: 本次打包的内容
+   * */ 
+  apply(compiler) {
+    // 异步方法，生成打包目录时：生成文件
+    compiler.hooks.emit.tapAsync('WebpackGitVersionPlugin', (compilation, callback) => {
+      try {
+        // 添加分支版本信息文件
+        let branchVersionObj = getGitVersionInfo()
+        if (!this.options.hiddenProjectName || !this.options.hiddenProjectVersion) {
+          // 获取package.json文件获取项目工程相关信息
+          const packageInfo = require(`${compilation.options.context}/package.json`)
+          branchVersionObj = {
+            projectName: packageInfo.name,
+            projectVersion: packageInfo.version,
+            ...branchVersionObj
+          }
+        }
+        
+        // 将打包版本信息添加到head中
+        if (!this.options.hiddenHead) {
+          let pkgInfoArr = []
+          let buildInfoArr = []
+          if (!this.options.hiddenBuildTime && branchVersionObj.buildTime) {
+            pkgInfoArr.push('buildTime:' + branchVersionObj.buildTime)
+          }
+          if (!this.options.hiddenProjectVersion && branchVersionObj.projectVersion) {
+            pkgInfoArr.push('version:' + branchVersionObj.projectVersion)
+          }
+          if (!this.options.hiddenProjectName && branchVersionObj.projectName) {
+            pkgInfoArr.push('project:' + branchVersionObj.projectName)
+          }
+          if (!this.options.hiddenBranchName && branchVersionObj.branchName) {
+            pkgInfoArr.push('branch:' + branchVersionObj.branchName)
+          }
+          if (!this.options.hiddenCommitId && branchVersionObj.commitId) {
+            pkgInfoArr.push('commit:' + branchVersionObj.commitId)
+          }
+
+          if (!this.options.hiddenBuildUserName && branchVersionObj.buildUserName) {
+            buildInfoArr.push('buildUser:' + branchVersionObj.buildUserName)
+          }
+          if (!this.options.hiddenBuildUserEmail && branchVersionObj.buildUserEmail) {
+            buildInfoArr.push('buildEmail:' + branchVersionObj.buildUserEmail)
+          }
+          if (!this.options.hiddenCommitTime && branchVersionObj.commitTime) {
+            buildInfoArr.push('commitTime:' + branchVersionObj.commitTime)
+          }
+          if (!this.options.hiddenCommitMsg && branchVersionObj.commitMsg) {
+            buildInfoArr.push('commitMsg:' + branchVersionObj.commitMsg)
+          }
+          if (!this.options.hiddenCommitAuthor && branchVersionObj.commitAuthor) {
+            buildInfoArr.push('commitAuthor:' + branchVersionObj.commitAuthor)
+          }
+          if (!this.options.hiddenCommitEmail && branchVersionObj.commitEmail) {
+            buildInfoArr.push('commitEmail:' + branchVersionObj.commitEmail)
+          }
+
+          let metaInfo = '<meta name="pkgInfo" content="' + pkgInfoArr.join(' ') + '">' +
+            '<meta name="revised" content="' + buildInfoArr.join(' ') + '">'
+          let assets = compilation.assets
+          let source = assets[this.options.indexFileName].source()
+          if (source.indexOf(this.options.replaceMeta) != -1) {
+            source = source.replace(this.options.replaceMeta, metaInfo)
+          } else {
+            source = source.replace('<head>', '<head>' + metaInfo)
+          }
+          assets[this.options.indexFileName] = {
+            source() {
+              return source
+            },
+            size() {
+              return source.length
+            }
+          }
+        }
+        
+        // 生成打包版本信息文件
+        if (this.options.isBuildFile) {
+          let versionInfoStr = JSON.stringify(branchVersionObj, null, 2)
+          compilation.assets[this.options.outputFile] = {
+            source: () => versionInfoStr,
+            size: () => versionInfoStr.length
+          }
+        }
+      } catch (error) {
+        console.log('WebpackGitVersionPlugin error:', error)
+      }
+      callback()
+    })
+  }
+}
+
+module.exports = WebpackGitVersionPlugin
